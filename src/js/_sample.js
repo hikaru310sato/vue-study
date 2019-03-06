7cf868431b0b825e7e12b45ad18680c9d06c0648
SATO hikaru <sato-hik@gnavi.co.jp>
2019-03-06 19:34:23 +0900

    [day3] 繰り返し表示を行うv-for

--- src/js/_sample.js
+++ src/js/_sample.js
@@ -7,11 +7,24 @@ export default () => {
       <input v-model="message">
       <button @click="increment">increment</button>
       <div v-bind:style="styleObject">{{messageAndCount}}</div>
+      <div>colors:
+        <span v-for="color in colors" :style="{color}">{{color}} </span>
+      </div>
+      <div>users:
+        <div v-for="user in users">
+          {{user.id}}:{{user.name}}
+        </div>
+      </div>
     </div>`,
     data() {
       return {
         message: 'Hello Vue.js !!',
         count: 0,
+        colors: ['red', 'blue', 'green'],
+        users: [
+          { id: 1, name: 'ユーザ１' },
+          { id: 2, name: 'ユーザ２' },
+        ],
       }
     },
     computed: {
